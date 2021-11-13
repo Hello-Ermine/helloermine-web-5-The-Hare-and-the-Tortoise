83cbce9a448d028b0495d137f8c173264c0f1a1c
Tle <warittinan.kg@mail.kmutt.ac.th>
2021-11-13 12:18:26 +0700

    หนูมลีมีลูกแมวเมียว

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import g1 from './gu/Ellipse79.png';
+import g2 from './gu/Ellipse80.png';
+import g3 from './gu/Ellipse81.png';
+import g4 from './gu/Ellipse82.png';
+import g5 from './gu/Ellipse83.png';
+import g6 from './gu/Ellipse84.png';
+import back from './gu/background.png';
+import styled from 'styled-components';
+const Photo =styled.div` 
+  background-image:url(${back});
+  height:10000px;
+  background-size: contain;
+  background-repeat: no-repeat;
+`
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div style={{backgroundImage:`url(${back})`,height:'800vh',backgroundSize:'contain'},backgroundRepeat:'no-repeat';}></div> */}
+        <Photo/>
+        <div className="g"> 
+        <img src={g1} alt=""/>
+        <img src={g2} alt=""/>
+        <img src={g3} alt=""/>
+        <img src={g4} alt=""/>
+        <img src={g5} alt=""/>
+        <img src={g6} alt=""/>
+        </div>
+
     </div>
   );
 }
