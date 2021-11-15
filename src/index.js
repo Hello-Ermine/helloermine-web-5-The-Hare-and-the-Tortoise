c94a5289d7fe58300500a734d7d6644c0d67d465
Saiparnn <chanakarn.sp@mail.kmutt.ac.th>
2021-11-15 19:10:31 +0700

    updateeeee

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
